1ce6fa66da983fd0374c75292f694aed21840232
Yaman Aswal <yaman@appsquad.com>
2024-05-12 13:49:11 +0530

    Init

---
+++ src/StarRating.js
@@ -0,0 +1,136 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+export default function StarRating({
+    maxRating = 5,
+    color = "#fcc419",
+    textSize = 22,
+    starSize = 44,
+    defaultRating = 0,
+    messages = []
+}) {
+
+    const [rating, setRating] = useState(defaultRating);
+    const [previewRating, setPreviewRating] = useState(0);
+
+    function handleRating(rating) {
+        setRating(rating);
+    }
+
+    const textStyle = {
+        margin: "0",
+        lineHeight: "1",
+        color: color,
+        fontSize: textSize
+    }
+
+    return (
+        <div style={containerStyle}>
+            <div style={starContainerStyle}>
+                {
+                    Array.from({ length: maxRating }, (_, i) => (
+                        <Star
+                            key={i}
+                            onRate={() => handleRating(i + 1)}
+                            full={previewRating ? previewRating >= i + 1 : rating >= i + 1}
+                            onHoverIn={() => setPreviewRating(i + 1)}
+                            onHoverOut={() => setPreviewRating(0)}
+                            color={color}
+                            starSize={starSize}
+                        />
+                    ))
+                }
+            </div>
+
+            <p style={textStyle}>
+                {messages.length === maxRating ? messages[previewRating ? previewRating - 1 : rating - 1] : previewRating || rating || ""}
+            </p>
+        </div>
+    );
+}
+
+
+function Star({ onRate, full, onHoverIn, onHoverOut, color, starSize }) {
+
+    const starStyle = {
+        width: starSize,
+        height: starSize,
+        display: "block",
+        cursor: "pointer"
+    }
+
+    return (
+        <div role="button"
+            onClick={onRate}
+            onMouseEnter={onHoverIn}
+            onMouseLeave={onHoverOut}>
+            {
+                full ?
+                    <FullStar
+                        color={color}
+                        starStyle={starStyle} /> :
+                    <EmptyStar
+                        color={color}
+                        starStyle={starStyle} />
+            }
+        </div>
+    );
+}
+
+function FullStar({ color, starStyle }) {
+    return (
+        <span style={starStyle}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill={color}
+                stroke={color}
+            >
+                <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                />
+            </svg>
+        </span>
+    );
+}
+
+function EmptyStar({ color, starStyle }) {
+    return (
+
+        <span style={starStyle}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke={color}
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="{2}"
+                    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                />
+            </svg>
+        </span>
+    );
+}
+
+const containerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '16px'
+}
+
+const starContainerStyle = {
+    display: 'flex',
+    gap: '4px'
+}
+
+StarRating.propTypes = {
+    maxRating: PropTypes.number,
+    defaultRating: PropTypes.number,
+    color: PropTypes.string,
+    messages: PropTypes.array,
+    textSize: PropTypes.number,
+    starSize: PropTypes.number,
+}
