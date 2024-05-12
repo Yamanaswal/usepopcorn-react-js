96a6ca55b69d35032eb1e0d93b82773ac89f2261
Yaman Aswal <yaman@appsquad.com>
2024-05-12 16:34:53 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import StarRating from './StarRating';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} color={'black'} messages={["Terrible", "Bad", "Okay", "Good", "Excellent"]} />
+    <App />
+    {/* <StarRating maxRating={5} color={'black'} messages={["Terrible", "Bad", "Okay", "Good", "Excellent"]} />
     <StarRating maxRating={10} color={'blue'} defaultRating={3} />
-    <StarRating textSize={80} />
+    <StarRating textSize={80} /> */}
   </React.StrictMode>
 );
 
